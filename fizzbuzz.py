284d1cc815085d293141f0c0d0d3f40025dce8ea
Abillash Mukundan (Abi) <94203598+Abillashmenon@users.noreply.github.com>
2023-07-25 18:56:58 -0400

    Update fizzbuzz.py
    fizzbuzz exercise with code updated

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,11 @@
 # add your code here
+for n in range(1,101):
+    if n % 3 == 0 and n % 5 == 0:
+        print("FIZZBUZZ")
+    elif n % 5 ==0:
+        print('Buzz')
+    elif n % 3 ==0:
+        print('Fizz')
+    else:
+        print(n)
 
